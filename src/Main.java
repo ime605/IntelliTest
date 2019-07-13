cb72a0bd8f22ab5dd77435acc7ee16d31bb4ae31
ime605 <1114841061@qq.com>
2019-07-13 14:45:48 +0800

    firstIDEAuse second test change

--- src/Main.java
+++ src/Main.java
@@ -3,5 +3,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello World!");
         System.out.println("first intelli project");
+        System.out.println("another test the change");
     }
 }
